59337f870ea9f2f42f74c23f85b3b48b173f80af
jazzari <passionjazz@gmail.com>
2020-05-27 15:33:10 +0200

    refactoring concerts_controller test

--- spec/controllers/concerts_controller_spec.rb
+++ spec/controllers/concerts_controller_spec.rb
@@ -3,22 +3,21 @@ require 'rails_helper'
 describe ConcertsController do 
 
 	describe '#index' do 
+		subject { get :index }
 
 		it 'should return success response' do
-		  get :index
+		  subject
 		  expect(response).to have_http_status(:ok) 
 		end
 
 		it 'should return proper json' do 
-			create_list :concert, 2
-			get :index
-			json = JSON.parse(response.body)
-			json_data = json['data']
-			expect(json_data.length).to eq(2)
-            expect(json_data[0]['attributes']).to eq({ "title" => "Live Concert Nº 1",
-              "description" => "Description of the Music Show Nº 1"}) 
-            expect(json_data[1]['attributes']).to eq({ "title" => "Live Concert Nº 2",
-              "description" => "Description of the Music Show Nº 2"})
+			concerts = create_list :concert, 2
+			subject
+			concerts.each_with_index do |concert, index|
+              expect(json_data[index]['attributes']).to eq({ 
+              	"title" => concert.title,
+                "description" => concert.description }) 
+            end
 		end
 
 	end
